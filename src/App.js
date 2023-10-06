98c7ce4b27c58befeacdcb63557a9538e2c4d920
Haneena22 <sweedanhaneen54@gmail.com>
2023-10-06 21:49:15 +0300

    Reactjs project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TicTacToc from './Components/TicTacToc/TicTacToc'
+function App() {
+  return (
+    <div>
+      <TicTacToc/>
+    </div>
+  );
+}
+
+export default App;
